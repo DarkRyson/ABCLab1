12413ced84861617f1da093e56c367da60eac511
DarkRyson <konovalov.nick.i.23@gmail.com>
2025-02-11 13:09:47 +0300

    Add files via upload

---
+++ ABCLab1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+setlocale(LC_ALL, "Russian");
+
+
+int main() {
+    std::string binNumber;
+    std::cout << "Введите число в двоичной системе счисления: ";
+    std::cin >> binNumber;
+
+    for (char ch : binNumber) {
+        if (ch != '0' && ch != '1') {
+            std::cout << "Ошибка,введено не двоичное число :(" << std::endl;
+            return;
+        }
+    }
+
+    if (binNumber.empty()) {
+        std::cout << "Ошибка,не было введено число" << std::endl;
+        return;
+    }
+
+    char lastB = binNumber.back();
+
+    if (lastB == '0') {
+        std::cout << "Число нечетное" << std::endl;
+    }
+    else {
+        std::cout << "Число четное" << std::endl;
+    }
+
+    return;
+}
